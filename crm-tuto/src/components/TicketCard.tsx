3535f4408c5131f859d99493f00748175ad21dd0
maicol <maykolandres4@gmail.com>
2023-02-13 21:28:52 -0500

    imported more stuff to the ticket card

--- crm-tuto/src/components/TicketCard.tsx
+++ crm-tuto/src/components/TicketCard.tsx
@@ -1,8 +1,26 @@
+import AvatarDisplay from './AvatarDisplay'
+import StatusDisplay from './StatusDisplay'
+import PriorityDisplay from './PriorityDisplay'
+import ProgressDisplay from './ProgressDisplay'
+import DeleteBlock from './DeleteBlock'
+import { Link } from 'react-router-dom'
+
 import React from 'react'
 
 const TicketCard = () => {
   return (
-    <div className=' ticket-card'>TicketCard</div>
+    <div className=' ticket-card'>
+      <div className="ticket-color">
+      <Link to={'/'}>
+         <h3>Title</h3>
+         <AvatarDisplay/>
+         <StatusDisplay/>
+         <PriorityDisplay/>
+         <ProgressDisplay/>
+      </Link>
+        <DeleteBlock/>
+      </div>
+    </div>
   )
 }
 
