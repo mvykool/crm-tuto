f590ba23bf24574698b549786209436f39656e96
maicol <maykolandres4@gmail.com>
2023-02-13 21:34:40 -0500

    creat ed object

--- crm-tuto/src/pages/Dashboard.tsx
+++ crm-tuto/src/pages/Dashboard.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 import TicketCard from '../components/TicketCard'
 
 const Dashboard = () => {
+
+
+  const tickets = [{
+     category: "q1, 2022",
+     color: "red",
+     title: "NFT safety 101",
+     owner: "ania kubow",
+     avatar: "https://www.freecodecamp.org/news/content/images/size/w150/2021/05/ania-kubow-gravatar.jpeg",
+     status: "done",
+     priority: "5",
+     progress: "40",
+     description: "make a video about that",
+     timestamp: "2022-02-11T07"
+  }]
+
   return (
     <div className='dashboard'>
       <h1>crm project</h1>
